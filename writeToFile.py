27046a19f1ea5746ab94d141ace8dd9dffb318a1
Michael Gardner <shellshocked2003@gmail.com>
2019-10-10 13:17:00 -0700

    Shortened path length

--- writeToFile.py
+++ writeToFile.py
@@ -4,11 +4,11 @@ import os
 counter = 0
 filePath = "./"
 
-for i in range(0,100):
+for i in range(0,10):
     filePath = filePath + "mirrorInTheBathroom" + str(i) + "/"
 
 if not os.path.exists(filePath):
     os.makedirs(filePath)
     
-with open(filePath + "PleaseTalkFree.txt", 'w') as longFile:
+with open(filePath + "PleaseTalkFreeTheDoorIsLockedJustYouAndMe.txt", 'w') as longFile:
     longFile.write("Wow, this is a really deep directory structure...\n")
